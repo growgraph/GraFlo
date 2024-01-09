0cece5ccfdb48dfc16c80f34f20d14bd415e792a
Alexander Belikov <abelikov@gmail.com>
2024-01-09 16:30:18 +0100

    simplied fetch

--- graph_cast/db/util.py
+++ graph_cast/db/util.py
@@ -1,17 +1,11 @@
-from arango.exceptions import CursorNextError
-
-
 def get_data_from_cursor(cursor, limit=None):
     batch = []
     cnt = 0
-    while True:
-        try:
-            if limit is not None and cnt >= limit:
-                raise StopIteration
-            item = next(cursor)
-            batch.append(item)
-            cnt += 1
-        except StopIteration:
-            return batch
-        except CursorNextError:
-            return batch
+
+    for item in cursor:
+        if limit is not None and cnt >= limit:
+            break
+        batch.append(item)
+        cnt += 1
+
+    return batch
