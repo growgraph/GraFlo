0db482f6a202472a51724e5006c0e63efa840f9e
Alexander Belikov <abelikov@gmail.com>
2024-01-10 20:03:18 +0100

    ingest tested

--- cli/ingest.py
+++ cli/ingest.py
@@ -1,4 +1,5 @@
 import logging.config
+import pathlib
 
 import click
 from suthing import ConfigFactory, FileHandle
@@ -11,18 +12,38 @@ logger = logging.getLogger(__name__)
 
 
 @click.command()
-@click.option("--db-config-path", type=click.Path())
-@click.option("--schema-path", type=click.Path())
-@click.option("--source-path", type=click.Path())
-@click.option("--limit-files", type=int | None, default=None)
-@click.option("-b", "--batch-size", default=5000, type=int)
-@click.option("--n-cores", default=1, type=int)
-@click.option("--n-threads", default=1, type=int)
+@click.option(
+    "--db-config-path",
+    type=click.Path(exists=True, path_type=pathlib.Path),
+    required=True,
+)
+@click.option(
+    "--schema-path",
+    type=click.Path(exists=True, path_type=pathlib.Path),
+    required=True,
+)
+@click.option(
+    "--source-path",
+    type=click.Path(exists=True, path_type=pathlib.Path),
+    required=True,
+)
+@click.option(
+    "--resource-pattern-config-path",
+    type=click.Path(exists=True, path_type=pathlib.Path),
+    default=None,
+)
+@click.option("--limit-files", type=int, default=None)
+@click.option("--batch-size", type=int, default=5000)
+@click.option("--n-cores", type=int, default=1)
+@click.option(
+    "--n-threads",
+    type=int,
+    default=1,
+)
 @click.option("--fresh-start", type=bool, help="wipe existing database")
-@click.option("--resource-pattern-config-path", type=click.Path())
 def ingest(
-    schema_path,
     db_config_path,
+    schema_path,
     source_path,
     limit_files,
     batch_size,
@@ -31,41 +52,37 @@ def ingest(
     fresh_start,
     resource_pattern_config_path,
 ):
-    name = schema_path.split(".")[-2]
-    logging.basicConfig(
-        format=(
-            "%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s:"
-            " %(message)s"
-        ),
-        datefmt="%Y-%m-%d %H:%M:%S",
-        level=logging.INFO,
-        filemode="w",
-    )
-    logging.config.fileConfig(
-        f"ingest_{name}.log", disable_existing_loggers=False
-    )
+    schema_path.name.split(".")[0]
+    logging.config.fileConfig(f"logging.conf", disable_existing_loggers=False)
+
+    logging.basicConfig(level=logging.INFO)
 
     schema = Schema.from_dict(FileHandle.load(fpath=schema_path))
-
-    patterns = Patterns.from_dict(
-        FileHandle.load(fpath=resource_pattern_config_path)
-    )
 
     conn_conf = ConfigFactory.create_config(
         dict_like=FileHandle.load(fpath=db_config_path)
     )
 
+    if resource_pattern_config_path is not None:
+        patterns = Patterns.from_dict(
+            FileHandle.load(fpath=resource_pattern_config_path)
+        )
+    else:
+        patterns = Patterns()
+
     schema.fetch_resource()
 
-    caster = Caster(schema)
+    caster = Caster(
+        schema,
+        n_cores=n_cores,
+        n_threads=n_threads,
+    )
     caster.ingest_files(
         path=source_path,
         limit_files=limit_files,
         clean_start=fresh_start,
         batch_size=batch_size,
         conn_conf=conn_conf,
-        n_cores=n_cores,
-        n_threads=n_threads,
         patterns=patterns,
     )
 
